743abc3a28cd258cecd4daacdec2dad1bc19f97e
Inês Veiga (1210852) <1210852@isep.ipp.pt>
2025-01-29 20:20:24 +0000

    Descrição Dados

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pylab as plt
+import seaborn as sns
 
 
 df = pd.read_parquet("dados_sensores_5000.parquet")
-
 print(df.head())
+
+#Entender a Estrutura dos Dados
+print(df.shape)
+print(df.head())
+print(df.columns)
+print(df.info())
+
+df.describe()
+
+df.query('agua_m3 > 200')
+
+#df.createOrReplaceTempView("sales")
+#resultado = spark.sql("SELECT Region, SUM(TotalPrice) AS TotalRevenue FROM sales GROUP BY Region")
+#resultado.show()
