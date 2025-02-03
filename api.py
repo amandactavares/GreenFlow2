c142c88cb1154f8a7bf65d12c894b82d94c30dcd
HCorte <hmmbcorte@gmail.com>
2025-02-03 12:24:34 +0000

    summary of the data read

--- api.py
+++ api.py
@@ -5,8 +5,8 @@ from db import create_table_from_excel
 
 app = Flask(__name__)
 
-@app.route("/readFields", methods=["GET"])
-def readFields():
+@app.route("/Summary", methods=["GET"])
+def readFileSummary():
     #Carregar DataSet
     if 'fileName' not in request.args:
         file_name = 'dados_sensores_5000.parquet'
@@ -14,8 +14,19 @@ def readFields():
         file_name = request.args['fileName']   
     df = pd.read_parquet(file_name)
     # Get column names
-    parquet_columns = df.columns
-    return parquet_columns.tolist()
+    parquet_columns = df.columns.tolist()
+    # Read the first row (index 0)
+    row = df.iloc[0]  # Returns a Series
+    print(row)
+    types = { }
+    for column in parquet_columns:
+        types[column]=type(row[column]).__name__
+
+    data = {
+        "columns": parquet_columns,
+        "types": types
+    }
+    return data
 
 @app.route("/parquettodb", methods=["POST"])
 def loadToDb():
